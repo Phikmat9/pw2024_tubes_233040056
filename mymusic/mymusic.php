a1e29a58d41867bbbc2908eac07026f51525336a
Phikmat9 <phikmat9@gmail.com>
2024-05-24 21:03:56 +0700

    progres+

--- mymusic/mymusic.php
+++ mymusic/mymusic.php
@@ -2,7 +2,7 @@
 
 require "functions.php";
 
-$mahasiswa= query("select * from music");
+$music= query("select * from music");
 ?>
 
 
@@ -17,7 +17,6 @@ $mahasiswa= query("select * from music");
   <body>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -25,23 +24,6 @@ $mahasiswa= query("select * from music");
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
       </ul>
       <form class="d-flex" role="search">
@@ -52,38 +34,44 @@ $mahasiswa= query("select * from music");
   </div>
 </nav>
 
-
 <div class="container">
-        <h1>MyMusic</h1>
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Judul</th>
-              <th scope="col">Artis</th>
-              <th scope="col">Album</th>
-              <th scope="col">Genre</th>
-              <th scope="col">Durasi</th>
-              <th scope="col">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php foreach($music as $msc) : ?>
-            <tr>
-              <td><?= $msc ['id']; ?></td>
-              <td><?= $msc ['judul']; ?></td>
-              <td><?= $msc ['artis']; ?></td>
-              <td><?= $msc ['album']; ?></td>
-              <td><?= $msc ['genre']; ?></td>
-              <td><?= $msc ['durasi']; ?></td>
-              <td>
-                <a href="hapus.php?id=<?= $msc["id"];?>"><span class="badge text-bg-danger">Hapus</span></a>
-              </td>
-            </tr>
-          </tbody>
-          </table>
-          <?php endforeach; ?>
-</div>
+  <h1>MyMusic</h1>
+
+  <a href="tambah.php" class="btn btn-primary">+</a>
+
+    <table class="table">
+
+
+    <div class="container">
+    <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Judul</th>
+        <th scope="col">Artis</th>
+        <th scope="col">Album</th>
+        <th scope="col">Genre</th>
+        <th scope="col">Durasi</th>
+        <th scope="col">Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach($music as $msc) : ?>
+        <tr>
+          <td><?= $msc ['id']; ?></td>
+          <td><?= $msc ['judul']; ?></td>
+          <td><?= $msc ['artis']; ?></td>
+          <td><?= $msc ['album']; ?></td>
+          <td><?= $msc ['genre']; ?></td>
+          <td><?= $msc ['durasi']; ?></td>
+          <td>
+            <a href="hapus.php?id=<?= $msc["id"];?>"><span class="badge text-bg-danger">Hapus</span></a>
+          </td>
+        </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+  </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
